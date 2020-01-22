d1285f77de404d06fa0069bf5d13aec0681ebc60
Aman Gupta <amangupta884036@gmail.com>
2020-01-22 12:43:59 +0530

    bug resolved

--- autogit.sh
+++ autogit.sh
@@ -1,49 +1,34 @@
 #This script should recursively traverse through the directories and commit every file
 
 #!/bin/bash
-if [ -z "$1" ]; then #This is to check that the argument is provided or not
-echo "No file provided" >&2
+
+[ $# -eq 1 ] && start=$1
+
+if [ ! -d $start ]
+then 
+echo "$start not a directory"
 exit 1
 fi
+dr=`pwd`
+Dir=$(find "$start" -type d)
+echo $Dir
 
-function func()
-{
-file_name=$1 
-if [ -d `realpath $file_name` ]; then
+for d in $Dir
+do
 
-cd `realpath $file_name`
+cd $dr/$d
 
-array=(`ls | uniq `)
-
-len=${#array[*]}
+arry=(`ls -p | grep -v / `)
+len=${#arry[*]}
 i=0
 while [ $i -lt $len ]; do
 
-echo "${array[$i]}"
+git add ${arry[$i]}
+git commit -m "${arry[$i]} is Automatically committed"
 
-func ${array[$i]}
 
 
 let i++
 done
 
-fi
-
-
-
-if [ -f `realpath $file_name` ]; then
-test= git add $file_name
-git commit -m "Automating commit"
-echo -e "\n#this was automatically comitted" >> $file_name
-git add $file_name
-git commit -m "Automated comment addition"
-
-if [ "$test" == "1" ]; then
-echo "Not executed properly" >&2
-exit 1
-fi
-
-
-}
-
-func $1
+done
